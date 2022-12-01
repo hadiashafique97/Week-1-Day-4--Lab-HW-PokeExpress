408aea17f8ec3296fa820b8ae8bde7bff9fef172
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2022-12-01 14:20:00 -0600

    index.js view rendered at pokemon route

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require('express')
 const app = express()
 var pokemon = require('./models/pokemon')
 const port = 3000
-
+app.set('view engine', 'jsx')
+app.engine('jsx',require('express-react-views').createEngine())
 
 app.get('/',(req, res) =>{
  res.send('<h4>Welcome to the Pokemon App!</h4>')
 })
 
 app.get('/pokemon', (req,res) =>{
-    res.send(pokemon)
+    res.render(res.render('Index', {allPokemon:pokemon
+        }))//line 4
 })
 
 
